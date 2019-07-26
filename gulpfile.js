a591319865bb692da5b481cf033cfb2308b4ca94
Rob Edmonds | Netmatters Ltd <rob.edmonds@netmatters.local>
2019-07-26 16:31:22 +0100

    Complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
-// import gulp from "gulp";
-// import sass from "gulp-sass";
-// import browserSync from "browser-sync"; //create()??
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const maps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
 
 function style() {
-    return gulp.src(["./scss/**/*.scss", "./scssNew/**/*.scss"])
+    return gulp.src(["./scss/**/*.scss"])
         .pipe(maps.init())
         .pipe(sass().on("error", sass.logError))
         .pipe(maps.write("./"))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
+}
+
+function concatScripts() {
+    return gulp.src("./js/*.js")
+        .pipe(maps.init())
+        .pipe(concat("all.js"))
+        .pipe(maps.write("./"))
+        .pipe(gulp.dest("./dist/"));
 }
 
 function watch() {
@@ -22,10 +28,14 @@ function watch() {
             baseDir: "./"
         }
     });
-    gulp.watch(["./scss/**/*.scss", "./scssNew/**/*.scss"], style);
+    gulp.watch(["./scss/**/*.scss"], style);
     gulp.watch("./*.html").on("change", browserSync.reload);
-    gulp.watch("/js/**/*.js").on("change", browserSync.reload);
+    gulp.watch("./js/**/*.js").on("change", function () {
+        concatScripts();
+        browserSync.reload()
+    });
 }
 
 exports.style = style;
 exports.watch = watch;
+exports.concatScripts = concatScripts;
